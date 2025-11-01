386178fda6449a9781648a6af719a43dd8fa8868
piacsek <felipe@piacsek.me>
2025-11-01 15:17:04 -0700

    homepage: vibes!

--- lib/elixir_lab_web/controllers/page_controller.ex
+++ lib/elixir_lab_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule ElixirLabWeb.PageController do
   use ElixirLabWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    random_id = :rand.uniform(1000)
+    image_url = "https://picsum.photos/400/400?random=#{random_id}"
+
+    render(conn, :home, image_url: image_url)
   end
 end
