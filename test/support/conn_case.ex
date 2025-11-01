344f82ec465fccaa5da6782ece6df4bbc103240c
piacsek <felipe@piacsek.me>
2025-11-01 12:33:01 -0700

    conn_case: untying ConnCase from DataCase

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -31,8 +31,7 @@ defmodule ElixirLabWeb.ConnCase do
     end
   end
 
-  setup tags do
-    ElixirLab.DataCase.setup_sandbox(tags)
+  setup _tags do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
