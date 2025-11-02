832fd7987db0e562c7ff2648df09598ff891c723
piacsek <felipe@piacsek.me>
2025-11-02 11:59:31 -0800

    adds credo

--- mix.exs
+++ mix.exs
@@ -63,7 +63,8 @@ defmodule ElixirLab.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
