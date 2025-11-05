eda0b9f9ae278f359a2a52f539e122fadbcba43a
piacsek <felipe@piacsek.me>
2025-11-05 10:05:51 -0800

    CI: testing concurrency settings

--- lib/elixir_lab_web/controllers/page_controller.ex
+++ lib/elixir_lab_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule ElixirLabWeb.PageController do
   use ElixirLabWeb, :controller
 
   def home(conn, _params) do
-    random_id = :rand.uniform(1000)
+    random_id = :rand.uniform(1001)
     image_url = "https://picsum.photos/400/400?random=#{random_id}"
 
     render(conn, :home, image_url: image_url)
